13791baf3daf1fb812b7375b6c417c3e4bbe7139
Sivanush <143810848+Sivanush@users.noreply.github.com>
2024-06-12 19:03:23 +0530

    Update user-auth.interceptor.ts

--- Client/src/app/services/interceptor/user-auth.interceptor.ts
+++ Client/src/app/services/interceptor/user-auth.interceptor.ts
@@ -3,7 +3,7 @@ import { HttpInterceptorFn } from '@angular/common/http';
 export const userAuthInterceptor: HttpInterceptorFn = (req, next) => {
   const token = localStorage.getItem('userToken');
   if(token){
-  console.log('chumma');
+  
   }
   const clonedRequest = req.clone({
     setHeaders: {
